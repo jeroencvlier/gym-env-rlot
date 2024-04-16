98dcb10f87667a1cc1c66ea894acf5e004e208a0
Jeroen van Lier <jeroencvlier@gmail.com>
2024-04-16 21:28:17 +0200

    Update artifact path and ticker in BuySellUndEnv class

--- src/gym_env_rlot/buy_sell/gym_env.py
+++ src/gym_env_rlot/buy_sell/gym_env.py
@@ -64,161 +64,116 @@ class BuySellUndEnv(gym.Env):
         self.test_train = config["test_train"]
         self.ticker = config["ticker"]
         self.artifact_path = config["artifact_path"]
-
-        self.noise_factor = 0.0
-        self.observation_noise_std = 0.0
         self.max_episode_steps = 500
-
-        if self.test_train == "train":
-            self.noise_factor = 0.05
-            self.observation_noise_std = 0.05
 
         self.data = load_data(self.artifact_path, self.ticker, self.test_train)
         self.feature_names = get_feature_names(self.artifact_path, self.ticker)
-        self.feature_names += ["pl","in_trade"]  # removed the "in_price" feature
+        self.feature_names += ["trade_pl", "in_trade"]  # removed the "in_price" feature
         self.action_space = Discrete(2)
-        
-        self.observation_space = Box(
-            low=-1.0, high=1.0, shape=(len(self.feature_names),), dtype=np.float32
-        )
 
-    def obs_(
-        self,
-    ):
-        pl = 0.0
-        if self.in_trade == 1:
-            pl = self.pl
-            if pl != 0.0:
-                pl = np.clip((pl / 500) - 1, -1, 1)
+        self.num_features = len(self.feature_names)
+        low = np.full((self.num_features,), -1.0, dtype=np.float32)
+        high = np.full((self.num_features,), 1.0, dtype=np.float32)
+        low[-1] = 0.0  # Adjust the last dimension to range from 0 to 1 for in_trade
+        self.observation_space = Box(low=low, high=high, dtype=np.float32)
 
-
-        self.observation = self.data[self.position]["data"]
+        self.noise_ratio = 0.0
         if self.test_train == "train":
-            self.observation += np.random.normal(
-                0, self.observation_noise_std, len(self.observation)
-            )
-            self.observation = np.clip(self.observation, -1, 1)
-        self.observation = np.append(self.observation, [self.in_trade, pl])
-        return self.observation
+            self.noise_ratio = 0.05
+
+    def obs_(self):
+        trade_pl = 0.0
+        if self.in_trade == 1:
+            trade_pl = self.trade_pl
+            if trade_pl != 0.0:
+                trade_pl = np.clip((trade_pl / 500) - 1, -1, 1)
+
+        self.obs = self.data[self.idx]["data"]
+        if self.test_train == "train":
+            self.obs += np.random.normal(0, self.noise_ratio, len(self.obs))
+            self.obs = np.clip(self.obs, -1, 1)
+        self.obs = np.append(self.obs, [trade_pl, self.in_trade])
+        return self.obs
+
+    def trade_pl_(self):
+        return self.data[self.idx]["underlyingPrice"] - self.entry_price
 
     def step(self, action):
         self.steps += 1
-        self.position += 1
+        self.idx += 1
         self.done = self.steps >= (self.max_episode_steps)
-        self.reward = self.total_pl
-
+        self.reward = self.total_pl  # TODO: Revisit this
         self.action = action
 
-        if self.action == 0:
-            # action 0 is to not be in a trade
-            if self.in_trade == 0:
-                # Not in a trade, no action taken
-                pass
-            elif self.in_trade == 1:
-                # In a trade, exit the trade
-                self.trade_pl = round(
-                    self.data[self.position]["underlyingPrice"]
-                    - self.entry_market_price,
-                    3,
-                )
-                self.pct_pl_running = round(
-                    self.pct_pl_running + (self.trade_pl / self.entry_market_price),
-                    6,
-                )
+        if self.action == 1:  # action 1 means enter/stay in trade
+            if self.in_trade == 0:  # Not in a trade, enter the trade
+                self.trade_count += 1
+                self.in_trade = 1
+                entry_noise = np.random.uniform(-self.noise_ratio, self.noise_ratio)
+                self.entry_price = self.data[self.idx - 1][
+                    "underlyingPrice"
+                ]  # + entry_noise # TODO: Uncomment this
 
+            # elif self.in_trade == 1:
+
+            # self.reward += self.trade_pl * 1  # TODO: Revisit this
+            # self.reward += self.trade_pl_pct  # TODO: Revisit this
+
+            self.trade_pl = self.trade_pl_()
+
+        if self.action == 0:  # action 0 means exit/stay out trade
+            if self.in_trade == 0:  # Not in a trade, no action taken
+                pass
+            elif self.in_trade == 1:  # In a trade, exit the trade
+                self.trade_pl = self.trade_pl_()
+                self.trade_pl_pct = self.trade_pl / self.entry_price
                 self.total_pl = round(self.total_pl + self.trade_pl, 3)
-                del self.entry_market_price
-                self.reward += self.total_pl + (self.trade_pl * 5)
-                if self.trade_pl < 0:
-                    self.reward -= 10
+                # self.reward += self.total_pl + (self.trade_pl * 1)
+                # if self.trade_pl < 0:
+                #     self.reward += 2 * self.trade_pl
                 self.in_trade = 0
                 self.trade_pl = 0.0
-                self.pl = 0.0
+                self.entry_price = np.nan
 
-            else:
-                # this is not possible but for clarity
-                raise ValueError("Invalid trade state")
-
-        elif self.action == 1:
-            # action 1 is to be in a trade
-            if self.in_trade == 0:
-                # Not in a trade, enter the trade
-                self.total_trades += 1
-                # add noise of between -0.05 and 0.05 to entry price
-                self.noise = round(
-                    np.random.uniform(-self.noise_factor, self.noise_factor), 3
-                )
-                self.entry_market_price = (
-                    self.data[self.position]["underlyingPrice"] + self.noise
-                )
-                self.in_trade = 1
-                self.pl = round(
-                    self.data[self.position]["underlyingPrice"]
-                    - self.entry_market_price,
-                    3,
-                )
-
-            elif self.in_trade == 1:
-                self.pl = round(
-                    self.data[self.position]["underlyingPrice"]
-                    - self.entry_market_price,
-                    3,
-                )
-                self.reward += self.total_pl * self.pct_pl_running
-                self.reward += self.pl * self.pct_pl_running
-            else:
-                # this is not possible but for clarity
-                raise ValueError("Invalid trade state")
-        else:
-            # not a valid action, statement for clarity
-            raise ValueError("Invalid action")
-
-        if self.done:
-            if self.total_trades < 5:
-                self.reward = -1000
-
-        self.running_pl = round(self.total_pl + self.pl, 3)
-        self.running_pl_adjusted = round(self.running_pl * self.pct_pl_running, 6)
         self.running_pl_list.append(self.running_pl)
-        self.running_pl_adjusted_list.append(self.running_pl_adjusted)
+        running_pl_pct = self.running_pl / self.data[self.idx]["underlyingPrice"]
+        self.running_pl_pct_list.append(running_pl_pct)
         self.drawdown = calculate_maximum_drawdown(self.running_pl_list)
-        self.drawdown_adjusted = calculate_maximum_drawdown(
-            self.running_pl_adjusted_list
-        )
+        self.drawdown_pct = calculate_maximum_drawdown(self.running_pl_pct_list)
 
-        self.reward -= self.drawdown_adjusted
+        # self.reward -= self.drawdown
+        # self.reward -= self.drawdown_pct
+
+        info = {"total_pl": self.total_pl, "trade_count": self.trade_count}
+
         self.reward = round(self.reward, 6)
-
-        truncated = False
-        info = {"total_pl": self.total_pl, "total_trades": self.total_trades}
-
-        return self.obs_(), round(self.reward, 10), self.done, truncated, info
+        self.trade_pl = round(self.trade_pl, 3)
+        self.running_pl = round(self.total_pl + self.trade_pl, 3)
+        return self.obs_(), self.reward, self.done, False, info
 
     def reset(self, *, seed=42, options=None):
         self.steps = 0
         self.in_trade = 0
-        self.position = 0
+        self.idx = 0
 
         # set the start position to a random position in the data
         if self.test_train == "train":
-            self.position = np.random.randint(
-                0, len(self.data) - self.max_episode_steps
-            )
+            self.idx = np.random.randint(0, len(self.data) - self.max_episode_steps)
 
-        self.total_pl = 0.0
-        self.total_trades = 0
+        self.trade_count = 0
         self.trade_pl = 0.0
-        self.pl = 0.0
+        self.total_pl = 0.0
         self.running_pl = 0.0
         self.running_pl_list = []
-        self.running_pl_adjusted_list = []
+        self.running_pl_pct_list = []
+        self.trade_pl_pct = 1.0
         self.done = False
-        self.pct_pl_running = 1.0
+        self.entry_price = np.nan
         return self.obs_(), {}
 
     def custom_logs(self):
         log = {
-            "trades": self.total_trades,
+            "trades": self.trade_count,
             "drawdown": self.drawdown,
             "total_pl": self.running_pl,
         }
@@ -226,29 +181,25 @@ class BuySellUndEnv(gym.Env):
 
     def render(self, mode="human"):
         print(
-            f"STEP: {self.steps}, Position: {self.position}, Action: {self.action}, In-Trade: {self.in_trade}, PL: {self.pl}, Trade PL: {self.trade_pl}, Total PL: {self.total_pl}, Total Trades: {self.total_trades}, reward: {self.reward}, Underlying Price: {self.data[self.position]['underlyingPrice']}"
+            f"STEP: {self.steps}, Position: {self.idx}, Action: {self.action}, In-Trade: {self.in_trade}, Trade PL: {self.trade_pl}, Total PL: {self.total_pl}, Total Trades: {self.trade_count}, reward: {self.reward}, Underlying Price: {self.data[self.idx]['underlyingPrice']}"
         )
 
     def env_state(self):
         # return a dict of values
         render_dict = {
             "step": self.steps,
-            "position": self.position,
+            "position": self.idx,
             "action": self.action,
             "in_trade": self.in_trade,
-            "pl": self.pl,
             "running_pl": self.running_pl,
-            "running_pl_adjusted": self.running_pl_adjusted,
-            "pct_pl_running": self.pct_pl_running,
+            "pct_pl_running": self.trade_pl_pct,
             "trade_pl": self.trade_pl,
             "total_pl": self.total_pl,
-            "total_trades": self.total_trades,
+            "trade_count": self.trade_count,
             "reward": self.reward,
             "drawdown": self.drawdown,
-            "drawdown_adjusted": self.drawdown_adjusted,
-            "underlying_price": np.round(
-                self.data[self.position]["underlyingPrice"], 3
-            ),
+            "underlying_price": np.round(self.data[self.idx]["underlyingPrice"], 3),
+            "entry_price": self.entry_price,
         }
         return render_dict
 
@@ -266,12 +217,12 @@ if __name__ == "__main__":
     for i in tqdm(range(1)):
         env_states = []
         obs = env.reset()
-        done = False
+        done = env.done
         action = 1
         while not done:
             # action = env.action_space.sample()
 
-            if env.steps % 2 == 0:
+            if env.steps % 4 == 0:
                 # alternate action between 0 and 1 every 5 steps
                 if action == 0:
                     action = 1
@@ -287,9 +238,29 @@ if __name__ == "__main__":
     pd.set_option("display.max_columns", None, "display.max_rows", 50)
 
     df = pd.DataFrame(env_states)
+    df["underlying_price_cstual"] = (
+        df["underlying_price"] - df["underlying_price"].iloc[0]
+    )
+    # calculate the difference from each row
+    df["row_diff"] = df["underlying_price_cstual"].diff()
 
-    # plot running_pl and running_pl_adjusted
-    df[["running_pl", "running_pl_adjusted"]].plot()
+    # plot running_pl and underlying_price_adj
+    df[["running_pl", "underlying_price_cstual"]].plot()
+
+    # line plot the rewards
+    df["reward"].plot()
 
     df.head(25)
-    df
+    display(
+        df[
+            [
+                "action",
+                "in_trade",
+                "trade_count",
+                "row_diff",
+                "trade_pl",
+                "underlying_price",
+                "underlying_price_cstual",
+            ]
+        ].head(25)
+    )
